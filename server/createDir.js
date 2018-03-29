6068895d9d205181f11f2866dd48326f256e7a1c
@akaJumi <imazjm@gmail.com>
2018-03-29 21:09:01 +0200

    server with module

---
+++ server/createDir.js
@@ -0,0 +1,31 @@
+const fs = require('fs-extra')
+const path = require('path')
+
+exports.create = (base, dirname) => {
+  const dir = path.resolve(base, dirname)
+  let response = {
+    path: dir,
+    created: false,
+    description: 'The directory: ' + dir + '/ already exists'
+  }
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, '0755')
+
+    if (fs.existsSync(dir)) {
+      response = {
+        path: dir,
+        created: true,
+        description: 'The directory: ' + dir + ' was succesfully created'
+      }
+    } else {
+      response = {
+        path: dir,
+        created: false,
+        description: 'Unable to create: ' + dir
+      }
+    }
+  }
+
+  return response
+}
