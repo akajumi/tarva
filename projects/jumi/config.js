92b9e0829943419dd9b32d3d6b2ab2fecdb52b78
@akaJumi <imazjm@gmail.com>
2018-03-30 16:03:09 +0200

    For testing.

--- projects/jumi/config.js
+++ projects/jumi/config.js
@@ -43,48 +43,6 @@ const config = options => {
         selectorExpansion: true,
         misMatchThreshold: 0.1,
         requireSameDimensions: true
-      },
-      {
-        label: 'Test project 1',
-        cookiePath:
-          'projects/' +
-          projectName +
-          '/backstop_data/engine_scripts/cookies.json',
-        url: 'file:///C:/TRABAJO/tarva/testProject/index.html',
-        referenceUrl: '',
-        readyEvent: '',
-        readySelector: '',
-        delay: 0,
-        hideSelectors: [],
-        removeSelectors: [],
-        hoverSelector: '',
-        clickSelector: '',
-        postInteractionWait: 0,
-        selectors: [],
-        selectorExpansion: true,
-        misMatchThreshold: 0.1,
-        requireSameDimensions: true
-      },
-      {
-        label: 'Test project 2',
-        cookiePath:
-          'projects/' +
-          projectName +
-          '/backstop_data/engine_scripts/cookies.json',
-        url: 'file:///C:/TRABAJO/tarva/testProject/index2.html',
-        referenceUrl: '',
-        readyEvent: '',
-        readySelector: '',
-        delay: 0,
-        hideSelectors: [],
-        removeSelectors: [],
-        hoverSelector: '',
-        clickSelector: '',
-        postInteractionWait: 0,
-        selectors: [],
-        selectorExpansion: true,
-        misMatchThreshold: 0.1,
-        requireSameDimensions: true
       }
     ],
     paths: {
